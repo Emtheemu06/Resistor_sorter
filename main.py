d38f18a936200ef850c0e4bcd49e08dab7cd8625
Emily Creed <creede@xavier.edu>
2026-09-15 17:03:22 -0400

    Starting code that has 2 completed functions. num_of_bands get the number of bands that the user sees. main is the main function. One inprogress function band_colors. I need to figure out how to get the input into a list.

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-#line just to get things started
+#importing numbers so I have easy access to some functions
+import numbers
+
+#setting up the lists to store the colors and values associted with the resistors
+colors_gen=["black","brown","red","oranage","yellow","green","blue","purple","gray","white"]
+digits=[0,1,2,3,4,5,6,7,8,9]
+multiplier=[1,10,100,1000,10000,100000,1000000,10000000,100000000,100000000]
+
+def num_of_bands():
+    band_num=input("How many bands does the resistor have? ")
+    while band_num.isdigit()==False:
+        band_num=input("Please enter a number (ie 1 or 2) ") 
+    if int(band_num)>=5:
+        print("Looks like you have a tolorance and possibly a temp coeff.")
+        band_num=4 #this is the max that the resistor can have that effects the math
+    return int(band_num)
+
+def band_colors(band_num):
+    colors_resistor=[]
+    
+def main():
+    print("test")
+    bandTest=num_of_bands()
+    print(bandTest)
+
+
+if __name__=="__main__" :
+    main()
