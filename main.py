f0f2284930c71e649564e6ecd285d19915e6682e
Emily Creed <creede@xavier.edu>
2026-09-17 17:29:26 -0400

    Color_check function was added to. Still needs to be completed. Tried to make style more consitant.

--- main.py
+++ main.py
@@ -1,46 +1,61 @@
-# importing numbers so I have easy access to some functions
-import numbers
-
-#setting up the lists to store the colors and values associted with the resistors
+# setting up the lists to store the colors and values associted with the resistors
 colors_gen = ["black","brown","red","oranage","yellow","green","blue","purple","gray","white"]
 digits = [0,1,2,3,4,5,6,7,8,9]
-multiplier = [1,10,100,1000,10000,100000,1000000,10000000,100000000,100000000]
-band_num = 0
+multiplier = [1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 100000000]
+num = 0
 
 def num_of_bands():
     band_num=input("How many bands does the resistor have? ")
     while band_num.isdigit()==False:
-        band_num=input("Please enter a number (ie 1 or 2) ") 
+        band_num=input("Please enter a number (ie 1 or 2) ")
     if int(band_num)>=5:
         print("Looks like you have a tolorance and possibly a temp coeff.")
-        band_num=4 #this is the max that the resistor can have that effects the math
+        # this is the max that the resistor can have that effects the math
+        band_num = 4
+    global num
+    num=int(band_num)
     return int(band_num)
 
 def band_colors(num):
-    #getting the colors from the user 
-    #calls color_check to then check those colors
+    # getting the colors from the user
+    # calls color_check to then check those colors
     print("There should be three or four bands that are closer together. ")
+    print("It is very important that when you list the colors in a moment, you do so with the colors in order.")
+    print("In no case should a black band be first.")
     user_colors=input("What are the colors of those bands on the resistor? Please enter the list separated by a space: ")
-    colors_resistor=user_colors.split()
+    colors_resistor_user=user_colors.split()
+    color_check(colors_resistor_user)
 
-def color_check(color_resisitor):
-#functions for making sure the colors given are valid
-    for color in color_resisitor:
-        match=False
-        while match ==False:
+def color_check(color_resisitor_list):
+# functions for making sure the colors given are valid
+    j = 0
+    black = "Black"
+    for color in color_resisitor_list:
+        match = False
+        i=0
+        while match == False:
             #insert for loop to loop through the other list and see if the colors match on in the list
+            i=0
             for baseColor in colors_gen:
-                if baseColor.casefold()==color.casefold():
-                    match=True
-            if match==False:
-                print("One of the colors you entered was invalid please try again")
-                band_colors(band_num)
-  
+                if baseColor.casefold() == color.casefold():
+                    if j == 0 and color.casefold() == black.casefold():
+                        print("Black can not be first")
+                        #add something to prompt user again
+                    else:
+                        match = True
+                if match ==False and i == 9:
+                    print("One of the colors you entered was invalid please try again. ")
+                    band_colors(num)
+                i = i+1
+        j = j+1
+    print("You colors match what I have as resistor colors")
+
+
 def main():
     print("test")
-    #bandTest=num_of_bands()
-    #print(bandTest)
+    bandTest = num_of_bands()
+    band_colors(bandTest)
 
 
-if __name__=="__main__" :
+if __name__ == "__main__":
     main()
