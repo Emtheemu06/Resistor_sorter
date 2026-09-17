2c75233ad86c61671d1f226457c46eae9e9b67d7
Emily Creed <creede@xavier.edu>
2026-09-17 11:58:58 -0400

    Color_check is almost completed still needs to be tested

--- main.py
+++ main.py
@@ -2,9 +2,10 @@
 import numbers
 
 #setting up the lists to store the colors and values associted with the resistors
-colors_gen=["black","brown","red","oranage","yellow","green","blue","purple","gray","white"]
-digits=[0,1,2,3,4,5,6,7,8,9]
-multiplier=[1,10,100,1000,10000,100000,1000000,10000000,100000000,100000000]
+colors_gen = ["black","brown","red","oranage","yellow","green","blue","purple","gray","white"]
+digits = [0,1,2,3,4,5,6,7,8,9]
+multiplier = [1,10,100,1000,10000,100000,1000000,10000000,100000000,100000000]
+band_num = 0
 
 def num_of_bands():
     band_num=input("How many bands does the resistor have? ")
@@ -15,7 +16,7 @@ def num_of_bands():
         band_num=4 #this is the max that the resistor can have that effects the math
     return int(band_num)
 
-def band_colors(band_num):
+def band_colors(num):
     #getting the colors from the user 
     #calls color_check to then check those colors
     print("There should be three or four bands that are closer together. ")
@@ -28,12 +29,17 @@ def color_check(color_resisitor):
         match=False
         while match ==False:
             #insert for loop to loop through the other list and see if the colors match on in the list
-
+            for baseColor in colors_gen:
+                if baseColor.casefold()==color.casefold():
+                    match=True
+            if match==False:
+                print("One of the colors you entered was invalid please try again")
+                band_colors(band_num)
   
 def main():
     print("test")
-    bandTest=num_of_bands()
-    print(bandTest)
+    #bandTest=num_of_bands()
+    #print(bandTest)
 
 
 if __name__=="__main__" :
